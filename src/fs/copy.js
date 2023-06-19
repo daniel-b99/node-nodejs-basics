893b8f5297834117937367dbc68360d3d5155d89
Daniel Bielecki <danielb99@onet.pl>
2023-06-19 21:10:48 +0200

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const srcDir = path.join('./', 'files');
+    const destDir = path.join('./', 'files_copy');
+
+    if (!fs.existsSync(srcDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    if (fs.existsSync(destDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        fs.mkdirSync(destDir);
+        const files = fs.readdirSync(srcDir);
+        files.forEach(file => {
+            const srcFile = path.join(srcDir, file);
+            const desFile = path.join(destDir, file);
+            fs.copyFileSync(srcFile, desFile)
+        }) 
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }    
 };
 
 await copy();
