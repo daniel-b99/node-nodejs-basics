59ed8d24a5ffc92a44dcfe147574e0f99fb970f3
Daniel Bielecki <danielb99@onet.pl>
2023-06-19 19:07:35 +0200

    feat: add create script

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import * as fs from "fs";
+import * as path from "path";
+
 const create = async () => {
-    // Write your code here 
+    const newFile = 'fresh.txt';
+    const newFileContent = 'I am fresh an young';
+    const folderPath = 'files'
+    const filePath = path.join(folderPath, newFile)
+    const error = 'FS operation failed'
+
+fs.access(filePath, fs.constants.F_OK, (err) => {
+  if (!err) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(filePath, newFileContent, (err) => {
+    if (err) {
+      console.error('Error during file creation', err);
+      return;
+    }
+
+    console.log('fresh.txt has been created!');
+  });
+});
+
 };
 
 await create();
